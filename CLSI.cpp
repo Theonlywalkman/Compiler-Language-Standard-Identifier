cf02f2484ce0868b774f9353d63b8a39d2c1cdc5
Walkman <Theonlywalkman@gmail.com>
2026-07-29 15:31:53 +0330

    got the code out of the main to work

--- CLSI.cpp
+++ CLSI.cpp
@@ -53,12 +53,12 @@ int main()
             std::cout << "Your compiler is using a preview/pre-release of " << stdName[i]
                 << " (language standard code " << standard << "L)\n";
             break;
-
-            std::cin.clear(); // reset any error flags
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
-            std::cin.get(); // get one more char from the user
         }
     }
+
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    std::cin.get(); // get one more char from the user
 
     return 0;
 }
